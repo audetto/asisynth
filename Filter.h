1542053887b5c39fcfa6b20a49455919d3f470fc
Andrea Odetti <mariofutire@gmail.com>
2016-02-14 15:44:59 +0000

    Filter optimisation: some trial and error shows this seems to perform better.
    Andrea

--- Filter.h
+++ Filter.h
@@ -26,9 +26,8 @@ namespace ASI
       m_a.fill(0.0);
 
       m_b[0] = 1.0;
-      m_a[0] = 1.0;
-      m_sizeOfB = 1;
-      m_sizeOfA = 1;
+      m_a[0] = 0.0;               // a is normalised!
+      m_sizeOfAB = 1;
     }
 
     virtual void init(std::vector<double> b, std::vector<double> a) override
@@ -43,42 +42,48 @@ namespace ASI
 	throw std::invalid_argument("IIR a is too large");
       }
 
+      if (a.empty())
+      {
+	throw std::invalid_argument("IIR a is empty");
+      }
+
+      m_b.fill(0.0);
+      m_a.fill(0.0);
       std::copy(b.begin(), b.end(), m_b.begin());
       std::copy(a.begin(), a.end(), m_a.begin());
 
-      m_sizeOfB = b.size();
-      m_sizeOfA = a.size();
+      m_sizeOfAB = std::max(b.size(), a.size());
 
       // normalise so we do not need to worry about m_a[0] later
-      for (size_t i = 1; i < m_sizeOfA; ++i)
+      for (size_t i = 1; i < m_sizeOfAB; ++i)
       {
 	m_a[i] /= m_a[0];
       }
+      m_a[0] = 0.0;
     }
 
     double process(const double x)
     {
       // add it
       m_x[m_pos & ((1 << N) - 1)] = x;
-      // now (till end of function m_posX is the last position written to)
+      // now (till end of function m_pos is the last position written to)
 
-      double y = 0;
-
-      for (ssize_t i = 0; i < m_sizeOfB; ++i)
+      double sum_y = 0.0;
+      double sum_x = 0.0;
+      for (size_t i = 0; i < m_sizeOfAB; ++i)
       {
-	// on the first iteration we read from m_posX
+	// on the first iteration we read from m_pos
 	// which has just been written to
 	const ssize_t k = (m_pos - i) & ((1 << N) - 1);
-	y += m_x[k] * m_b[i];
+	sum_x += m_x[k] * m_b[i];
+
+	// on the first iteration m_a[0] = 0
+	// the,the next we read from m_pos - 1
+	// which was written to on the previous call to ::process()
+	sum_y += m_y[k] * m_a[i];
       }
 
-      for (ssize_t i = 1; i < m_sizeOfA; ++i)
-      {
-	// on the first iteration we read from m_posY - 1
-	// which was written to on the previous call to ::process()
-	const ssize_t k = (m_pos - i) & ((1 << N) - 1);
-	y -= m_y[k] * m_a[i];
-      }
+      const double y = sum_x - sum_y;
 
       // write to
       m_y[m_pos & ((1 << N) - 1)] = y;
@@ -95,8 +100,7 @@ namespace ASI
     std::array<double, 1 << N> m_x;
     std::array<double, 1 << N> m_y;
 
-    size_t m_sizeOfB;
-    size_t m_sizeOfA;
+    size_t m_sizeOfAB;
     std::array<double, 1 << N> m_b;
     std::array<double, 1 << N> m_a;
   };
